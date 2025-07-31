8f8b4d1d34e8cc2971f3335d1978968f39f2f7f5
IlTuoConsulenteIT <141905327+iltuoconsulenteit@users.noreply.github.com>
2025-07-31 15:36:46 +0200

    Fix User model primary key

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,8 +1,9 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Integer
 from app.auth.database import Base
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
     license_type = Column(String(length=10), default="free")
     license_token = Column(String(length=64), nullable=True)
