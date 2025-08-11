1168d85ded074827c611c1b9d60f81c46184f81a
IlTuoConsulenteIT <141905327+iltuoconsulenteit@users.noreply.github.com>
2025-08-11 17:04:58 +0200

    refactor: remove joomla module and add template

--- mod_doccropper_expressive/tmpl/partials/appbar.php
+++
@@ -1,4 +0,0 @@
-<header class="flex items-center p-4 bg-gray-800 text-white">
-  <button class="mr-4 md:hidden" @click="drawer = true">☰</button>
-  <h1 class="text-lg font-semibold">DocCropper</h1>
-</header>
--- mod_doccropper_expressive/tmpl/partials/toasts.php
+++
@@ -1,3 +0,0 @@
-<div x-show="toast" x-transition class="fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded">
-  <span x-text="toast"></span>
-</div>
