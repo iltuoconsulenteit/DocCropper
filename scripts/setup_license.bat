da0e7918d71df0237ee81f8f0b5421cac942aa8a
IlTuoConsulenteIT <141905327+iltuoconsulenteit@users.noreply.github.com>
2025-08-25 17:24:40 +0200

    fix: stabilize Windows license setup

--- scripts/setup_license.bat
+++ scripts/setup_license.bat
@@ -17,12 +17,12 @@ if not "%~1"=="am_admin" (
 
 :: Determine application directory (where main.py resides)
 set "SCRIPT_DIR=%~dp0"
-if exist "!SCRIPT_DIR!main.py" (
-    set "APP_DIR=!SCRIPT_DIR!"
+if exist "%SCRIPT_DIR%main.py" (
+    set "APP_DIR=%SCRIPT_DIR%"
 ) else (
-    set "APP_DIR=!SCRIPT_DIR!\.."
+    set "APP_DIR=%SCRIPT_DIR%.."
 )
-cd /d "!APP_DIR!"
+cd /d "%APP_DIR%"
 
 if not exist env mkdir env
 
@@ -34,34 +34,38 @@ if exist scripts\stop_DocCropper.bat (
 
 set /p LICENSE_KEY=Enter license key:
 set /p LICENSE_NAME=Enter license name:
->>"%LOGFILE%" echo %date% %time% Key entered
+>>"%LOGFILE%" echo %date% %time% Key entered !LICENSE_KEY! for !LICENSE_NAME!
 
 set "ENVFILE=env\developer.env"
 
-{
+(
     echo DOCROPPER_LICENSE_KEY=!LICENSE_KEY!
     echo DOCROPPER_LICENSE_NAME=!LICENSE_NAME!
     echo DOCROPPER_DEV_LICENSE=!LICENSE_KEY!
     echo DOCROPPER_LICENSE_LEVEL=developer
     echo DOCROPPER_DEV_PASSWORD=87654321
     echo DOCROPPER_SETTINGS_PASSWORD=12345678
-} >"!ENVFILE!" && >>"%LOGFILE%" echo %date% %time% Wrote !ENVFILE!
+) >"%ENVFILE%" && >>"%LOGFILE%" echo %date% %time% Wrote %ENVFILE%
 
-python - <<PY && >>"%LOGFILE%" echo %date% %time% Updated settings.json
-import json,sys
-path='settings.json'
-try:
-    data=json.load(open(path))
-except Exception:
-    data={}
-data['license_key']=r'%LICENSE_KEY%'
-data['license_name']=r'%LICENSE_NAME%'
-data['license_level']='developer'
-json.dump(data,open(path,'w'),indent=2)
-PY
+set "PYFILE=%TEMP%\docc_update.py"
+(
+    echo import json, os
+    echo path = 'settings.json'
+    echo try:
+    echo     data = json.load(open(path))
+    echo except Exception:
+    echo     data = {}
+    echo data['license_key'] = os.environ.get('LICENSE_KEY','')
+    echo data['license_name'] = os.environ.get('LICENSE_NAME','')
+    echo data['license_level'] = 'developer'
+    echo json.dump(data, open(path,'w'), indent=2)
+) >"%PYFILE%"
+python "%PYFILE%" && >>"%LOGFILE%" echo %date% %time% Updated settings.json
+del "%PYFILE%"
+
 if exist license_overrides.json del license_overrides.json
 
-echo Developer license saved to !ENVFILE! and settings.json
+echo Developer license saved to %ENVFILE% and settings.json
 echo Log written to %LOGFILE%
 >>"%LOGFILE%" echo %date% %time% License setup completed
 pause
