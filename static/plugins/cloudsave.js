97e2c0ae065347dc0a61a70e14859c0ce3833483
IlTuoConsulenteIT <141905327+iltuoconsulenteit@users.noreply.github.com>
2025-09-05 08:45:24 +0200

    Hide cloud save buttons and remove local save option

--- static/plugins/cloudsave.js
+++ static/plugins/cloudsave.js
@@ -3,14 +3,7 @@ export function initCloudSavePlugin(translations, enabled = true) {
         return;
     }
 
-    const localBtn = document.getElementById('localSaveBtn');
     const cloudBtn = document.getElementById('cloudSaveBtn');
-
-    if (localBtn) {
-        localBtn.addEventListener('click', () => {
-            alert(translations.localSaveComingSoon || 'Local saving coming soon');
-        });
-    }
 
     if (cloudBtn) {
         cloudBtn.addEventListener('click', () => {
